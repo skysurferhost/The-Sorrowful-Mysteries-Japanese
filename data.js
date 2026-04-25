7f5a85244ea3c5caf5dccbd66eaa2b35bf43bb21
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-04-25 14:39:22 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-lo-bun--m-hi-mu",
+      "name": "一、イエスはゲッセマネで苦しまれる",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8763970383256172,
+          "pitch": 0.16735844249532406,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-chu-nh-n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8315975342972379,
+          "pitch": -0.1932127154048935,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエスはゲッセマネで苦しまれる</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">それから、イエスは弟子たちと一緒にゲツセマネという所に来て、「私が向こうへ行って祈っている間、ここに座っていなさい」と言われた。ペトロとゼベダイの子二人とを伴われたが、苦しみ悩み始められた。そして、彼らに言われた。「私は死ぬほど苦しい。ここを離れず、私と共に目を覚ましていなさい。」少し先に進んでうつ伏せになり、祈って言われた。「父よ、できることなら、この杯を私から過ぎ去らせてください。しかし、私の望むようにではなく、御心のままに。」それから、弟子たちのところへ戻って御覧になると、彼らは眠っていたので、ペトロに言われた。「あなたがたはこのように、一時も私と共に目を覚ましていられなかったのか。誘惑に陥らぬよう、目を覚まして祈っていなさい。心ははやっても、肉体は弱い。」さらに、二度目に向こうへ行って祈られた。「父よ、私が飲まないかぎりこの杯が過ぎ去らないのでしたら、御心が行われますように。」再び戻って御覧になると、弟子たちは眠っていた。まぶたが重くなっていたのである。そこで、彼らを離れ、また向こうへ行って、三度目も同じ言葉で祈られた。それから、弟子たちのところに戻って来て言われた。「まだ眠っているのか。休んでいるのか。時が近づいた。人の子は罪人たちの手に渡される。立て、行こう。見よ、私を裏切る者が近づいて来た。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マタイ 26:36–46)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-chu-nh-n",
+      "name": "二、イエスはむちで打たれる",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03994914720259146,
+          "pitch": -0.13235010662935842,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-chu-i-mo-gai"
+        },
+        {
+          "yaw": -2.3599925639905557,
+          "pitch": -0.09310296493766934,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-lo-bun--m-hi-mu"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0021049083788469147,
+          "pitch": -0.4432680786781624,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエスはむちで打たれる</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">一夜が明けるとすぐ、祭司長たちは、長老たちや律法学者たちと共に最高法院全体で協議した後、イエスを縛って連れ出し、ピラトに引き渡した。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">ピラトがイエスに、「お前はユダヤ人の王なのか」と尋問すると、イエスは、「それは、あなたが言っていることだ」とお答えになった。そこで祭司長たちが、いろいろとイエスを訴えた。ピラトが再び尋問した。「何も答えないのか。あんなにお前を訴えているのに。」しかし、ピラトが不思議に思うほどに、イエスはもう何もお答えにならなかった。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">ところで、祭りの度に、ピラトは、人々が願い出る囚人を一人釈放していた。さて、暴動のとき人殺しをして投獄されていた暴徒の中に、バラバと言う男がいた。群衆が押しかけて来て、いつものようにしてほしいと要求し始めた。ピラトは、「あのユダヤ人の王を釈放してほしいのか」と言った。祭司長たちがイエスを引き渡したのは、妬みのためだと分かっていたからである。しかし、祭司長たちは、バラバのほうを釈放してもらうように群衆を扇動した。そこで、ピラトは改めて、「それでは、ユダヤ人の王とお前たちが言っているあの者は、どうしてほしいのか」と言った。群衆はまた叫んだ。「十字架につけろ。」ピラトは、「一体、どんな悪事を働いたというのか」と言ったが、群衆はますます激しく、「十字架につけろ」と叫んだ。ピラトは群衆を満足させようと思って、バラバを釈放した。そして、イエスを鞭打ってから、十字架につけるために引き渡した。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マルコ 15:1-15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-chu-i-mo-gai",
+      "name": "三、イエスはいばらの冠を受ける",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.770374617292628,
+          "pitch": -0.011154715826149797,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-vc-cy-thnh-gi"
+        },
+        {
+          "yaw": -3.1385039082777517,
+          "pitch": -0.14273786458115012,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-chu-nh-n"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7943040273107442,
+          "pitch": -0.21542148670605954,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエスはいばらの冠を受ける</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">兵士たちは邸宅、すなわち総督官邸の中にイエスを連れて行き、部隊の全員を呼び集めた。そして、イエスに紫の衣を着せ、茨の冠を編んでかぶらせ、「ユダヤ人の王、万歳」と挨拶し始めた。また、葦の棒で頭を叩き、唾を吐きかけ、ひざまずいて拝んだりした。このようにイエスを侮辱したあげく、紫の衣を脱がせて元の上着を着せた。そして、十字架につけるために外へ引き出した。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マルコ 15:16–20)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-vc-cy-thnh-gi",
+      "name": "四、イエスは十字架をになって歩まれる",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6983594634984662,
+          "pitch": -0.37386622268201464,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-chu-cht-trn-cy-thnh-gi"
+        },
+        {
+          "yaw": 3.1374699261000316,
+          "pitch": 0.2915448346724503,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-chu-i-mo-gai"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.66061327614063,
+          "pitch": -0.6363281998126844,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエスは十字架をになって歩まれる</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">人々はイエスを引いて行く途中、シモンというキレネ人が畑から帰って来るのを捕まえて、十字架を背負わせ、イエスの後から付いて行かせた。大勢の民衆と嘆き悲しむ女たちとが、イエスに従った。イエスは女たちの方を振り向いて言われた。「エルサレムの娘たち、私のために泣くな。自分と自分の子どもたちのために泣け。人々が、『不妊の女、子を生んだことのない胎、乳を飲ませたことのない乳房は幸いだ』と言う日が来るからである。その時、人々は山に向かって／『我々の上に崩れ落ちよ』と言い／丘には／『我々を覆え』と言い始める。生木でさえこうされるなら、枯れ木は一体どうなることだろう。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(ルカ 23:26–31)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-chu-cht-trn-cy-thnh-gi",
+      "name": "五、イエスは十字架の上で亡くなられる",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7763165764238629,
+          "pitch": 0.39931217659419715,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-vc-cy-thnh-gi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4895233345042449,
+          "pitch": -0.6379339964485311,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエスは十字架の上で亡くなられる</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">そこで、ピラトは、十字架につけるために、イエスを人々に引き渡した。</span><o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;"><b><i>十字架につけられる</i></b></span><o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">こうして、人々はイエスを引き取った。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">イエスは自ら十字架を背負い、いわゆる「されこうべの場所」、すなわちヘブライ語でゴルゴタという所へ向かわれた。そこで、彼らはイエスを十字架につけた。また、イエスと一緒にほかの二人を、イエスを真ん中にして両側に、十字架につけた。ピラトは罪状書きも書いて、十字架の上に掛けた。それには、「ナザレのイエス、ユダヤ人の王」と書いてあった。イエスが十字架につけられた場所は都に近かったので、多くのユダヤ人がその罪状書きを読んだ。それは、ヘブライ語、ラテン語、ギリシア語で書かれていた。ユダヤ人の祭司長たちはピラトに、「『ユダヤ人の王』と書かずに、『この男は「ユダヤ人の王」と自称した』と書いてください」と言った。ピラトは、「私が書いたものは、書いたままにしておけ」と答えた。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">兵士たちはイエスを十字架につけてから、その服を取り、四つに分け、各自に一つずつ渡るようにした。下着も取ってみたが、それには縫い目がなく、上から下まで一枚織りであった。そこで、「これは裂かないで、誰のものになるか、くじを引こう」と話し合った。それは、／「彼らは私の服を分け合い／衣をめぐってくじを引いた」という聖書の言葉が実現するためであった。兵士たちはこのとおりにしたのである。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">イエスの十字架のそばには、その母と母の姉妹、クロパの妻マリアとマグダラのマリアとが立っていた。イエスは、母とそのそばにいる愛する弟子とを見て、母に、「女よ、見なさい。あなたの子です」と言われた。それから弟子に言われた。「見なさい。あなたの母です。」その時から、この弟子はイエスの母を自分の家に引き取った。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">この後、イエスは、すべてのことが今や成し遂げられたのを知り、「渇く」と言われた。こうして、聖書の言葉が実現した。そこには、酢を満たした器が置いてあった。人々は、この酢をいっぱい含ませた海綿をヒソプに付け、イエスの口元に差し出した。イエスは、この酢を受けると、「成し遂げられた」と言い、頭を垂れて息を引き取られた。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(ヨハネ 19:16–30)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
